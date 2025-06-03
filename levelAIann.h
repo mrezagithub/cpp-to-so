41f4be265eda5088732b1377fcc6053f654632ae
mrezagithub <79081653+mrezagithub@users.noreply.github.com>
2025-06-03 09:31:08 +0330

    Update levelAIann.h

--- levelAIann.h
+++ levelAIann.h
@@ -1,5 +1,14 @@
-﻿#pragma once
+#pragma once
 #include <vector>
+#include <limits>
+#include <ctime>
+
+// Export macro برای Windows و Linux
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT
+#endif
 
 extern int Depth;
 extern double Point;
@@ -13,12 +22,10 @@ extern std::vector<double> highestBuf;
 extern std::vector<double> lowestBuf;
 
 constexpr double INF = std::numeric_limits<double>::infinity();
-const std::tm ZTIME = { 0,0,0,2,3,126,0,0,-1 };
+
 const double PI = 3.14159265358979323846;
 const double RADIAN2DEGREE = 180.0 / PI;
 const double DEGREE2RADIAN = PI / 180.0;
-double action = 0;
-
 
 struct Statis {
     int N = 0;
@@ -31,29 +38,16 @@ struct Statis {
     double RegBr = 0.0;
 };
 
-extern "C" __declspec(dllexport)
-Statis RunANN(const long* timeArr, const double* openArr, const double* highArr, const double* lowArr, const double* closeArr, int size,int digits, double point, int depth = 14);
+extern "C" EXPORT
+Statis RunANN(const long* timeArr, const double* openArr, const double* highArr, const double* lowArr, const double* closeArr, int size, int digits, double point, int depth = 14);
 
 int InitializeAll(int Bars, int depth, int digits, double point);
-
 int FindFirstExtremum(double& curlow, double& curhigh, int& whatlookfor, int& limit);
-
 int Lowest(const std::vector<double>& priceArray, int count, int start);
-
 int Highest(const std::vector<double>& priceArray, int count, int start);
-
-int LawnPeak(double& lasthigh, double& lastlow,
-    const std::vector<double>& high,
-    const std::vector<double>& low, int limit);
-
-int FinalCutting(double& curlow, double& curhigh,
-    double& lastlow, double& lasthigh,
-    int& whatlookfor,
-    const std::vector<double>& high,
-    const std::vector<double>& low, int limit);
-
+int LawnPeak(double& lasthigh, double& lastlow, const std::vector<double>& high, const std::vector<double>& low, int limit);
+int FinalCutting(double& curlow, double& curhigh, double& lastlow, double& lasthigh, int& whatlookfor, const std::vector<double>& high, const std::vector<double>& low, int limit);
 int CalculateLawnPeak(const std::vector<double>& high, const std::vector<double>& low);
-
-double QuantStatsNoneZero(const std::vector<double>& fx, Statis& stc,int offset, int zlimit, double empty_value = 0.0, int direction = -1);
-
+double QuantStatsNoneZero(const std::vector<double>& fx, Statis& stc, int offset, int zlimit, double empty_value = 0.0, int direction = -1);
 double DateTimeStruct(const long* times, int size);
+bool TimeAnalyze(std::tm& inputTime);
